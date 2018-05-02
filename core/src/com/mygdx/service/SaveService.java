90ba8c1a29e7b698d30e7e18eb8e37faf0df2bd5
apuskarczyk <apuskarczyk@iocean.fr>
2018-05-02 14:06:41 +0200

    rename

--- core/src/com/mygdx/service/SaveService.java
+++ core/src/com/mygdx/service/SaveService.java
@@ -24,6 +24,33 @@ public class SaveService {
 	}
 
 	public SaveService() {
+		initSaveFile();
+	}
+
+	public void loadAccount(int accountId) {
+		Path path = Paths.get(SAVE_PATH);
+		if (path.toFile().exists()) {
+			Gdx.app.log("SaveService", "File exists !");
+			try {
+				BufferedReader reader = new BufferedReader(new FileReader(SAVE_PATH));
+				String content = reader.readLine();
+				Gdx.app.log("reader", content);
+				for (int accountIndex = 0; accountIndex < NB_SAVE_PER_FILE; accountIndex++) {
+					for (int valueIndex = 0; valueIndex < NB_VALUE_SAVE; valueIndex++) {
+						Gdx.app.log("reader", "value " + valueIndex + " : "
+								+ Long.parseLong(content.substring((valueIndex * 8), (valueIndex * 8) + 8), 16));
+					}
+				}
+				reader.close();
+			} catch (FileNotFoundException e) {
+				Gdx.app.error("SaveService", "loadAccount FileNotFoundException ! ", e);
+			} catch (IOException e) {
+				Gdx.app.error("SaveService", "loadAccount IOException !", e);
+			}
+		}
+	}
+
+	private void initSaveFile() {
 		Path path = Paths.get(SAVE_PATH);
 		if (!path.toFile().exists()) {
 			Gdx.app.log("SaveService", "File don't exists !");
@@ -36,30 +63,6 @@ public class SaveService {
 				writer.close();
 			} catch (IOException e) {
 				Gdx.app.error("SaveService", "SaveService IOException !", e);
-			}
-		}
-		loadAccount(1);
-	}
-
-	public void loadAccount(int accountId) {
-		Path path = Paths.get(SAVE_PATH);
-		if (path.toFile().exists()) {
-			Gdx.app.log("SaveService", "File exists !");
-			try {
-				BufferedReader reader = new BufferedReader(new FileReader(SAVE_PATH));
-				String content = reader.readLine();
-				Gdx.app.log("reader", content);
-				for (int save = 0; save < NB_SAVE_PER_FILE; save++) {
-					for (int indexValue = 0; indexValue < NB_VALUE_SAVE; indexValue++) {
-						Gdx.app.log("reader", "value " + indexValue + " : "
-								+ Long.parseLong(content.substring((indexValue * 8), (indexValue * 8) + 8), 16));
-					}
-				}
-				reader.close();
-			} catch (FileNotFoundException e) {
-				Gdx.app.error("SaveService", "loadAccount FileNotFoundException ! ", e);
-			} catch (IOException e) {
-				Gdx.app.error("SaveService", "loadAccount IOException !", e);
 			}
 		}
 	}
