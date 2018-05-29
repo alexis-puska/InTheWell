4e1cfdf0c4036e22b7a9f2ad386c66c31cdc993d
apuskarczyk <apuskarczyk@iocean.fr>
2018-05-29 17:31:08 +0200

    player state

---
+++ core/src/com/mygdx/enumeration/PlayerState.java
@@ -0,0 +1,29 @@
+package com.mygdx.enumeration;
+
+public enum PlayerState {
+	//@formatter:off
+	WAIT,
+	WALK,
+	JUMP,
+	START_FALL,
+	FALL,
+	LANDING,
+	KILL,
+	SHOT_BOMBE,
+	DROP_BOMBE,
+	PLATFORME_EDGE,
+	BORING,
+	CHEWING_GUM,
+	KNOCK_OUT,
+	RAISE_UP,
+	RESPAWN,
+	ARM_UP,
+	RUN,
+	SOCCER_RUN,
+	SOCCER_ROLL,
+	VICTORY,
+	CARRY_BOMBE,
+	CRY,
+	SADNESS;
+	//@formatter:on
+}
